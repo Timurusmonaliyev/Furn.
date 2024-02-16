8f4696ad2b1aa7d0963a2b002e4f1e54680ac024
Timur <usmonaliyevtemur57@gmail.com>
2024-02-16 21:08:51 +0500

    yanatuzatildi

--- src/Views/AkmaljonCart/index.jsx
+++ src/Views/AkmaljonCart/index.jsx
@@ -4,8 +4,8 @@ import Rasm2 from "../../assets/populer-products/p2.png";
 import Rasm3 from "../../assets/populer-products/p3.png";
 const AkmaljonCart = () => {
   return (
-    <div className="grid md:grid gap-10 mt-44 w-[60%] md:w-[90%] m-auto lg:flex xl:flex xl:gap-10 my-10 px-0 lg:px-52 xl:px-56">
-      <div className="pb-0 xl:py-12 w-full xl:h-[290px] md:w-[50%] m-auto xl:w-[34%] bg-[#f8f9fc] flex justify-center">
+    <div className="grid md:grid mr-44 md:mr-16 lg:mr-20 md:grid-cols-1 gap-10 mt-44 w-[60%] lg:w-[95%] lg:ml-9 md:w-[90%] m-auto lg:flex xl:flex xl:gap-10 my-10 px-0 lg:px-0 xl:px-56">
+      <div className="pb-0 xl:py-12 h-[270px] pt-14 w-[280px] xl:h-[300px] lg:h-[280px] lg:w-[100%]  md:w-[50%] m-auto xl:w-[34%] bg-[#f8f9fc] flex justify-center">
         <div className="text-center">
           <div className="flex justify-center w-[80%] m-auto">
             <img src={Rasm1} alt="" />
@@ -14,14 +14,14 @@ const AkmaljonCart = () => {
             <h4>Arm Chair</h4>
           </div>
           <div>
-            <p className="text-[14px] text-[#a09e9c] xl:mx-auto w-[92%]">
+            <p className="text-[14px] text-[#a09e9c] mx-auto w-[92%]">
               Nemo enim ipsam voluptatem quia volu ptas sit asperna aut odit aut
             </p>
           </div>
         </div>
       </div>
-      <div className="xl:py-11 lg:py-10 w-[100%] md:w-[50%] m-auto xl:w-[60%] lg:w-[80%] bg-[#f8f9fc] flex justify-center">
-        <div className="text-start xl:flex xl:ml-10 grid xl:w-[90%]">
+      <div className="xl:py-11  md:p-8 lg:py-[17px] lg:p-0 w-[100%] md:w-[50%] m-auto xl:w-[60%] lg:w-[180%] bg-[#f8f9fc] flex justify-center">
+        <div className="text-start xl:flex lg:flex xl:ml-10 grid lg:w-full xl:w-[90%]">
           <div className="flex justify-center xl:w-[70%] lg:w-[100%]">
             <img
               className="xl:w-[100%] xl:mt-10 lg:h-[150px] "
@@ -29,12 +29,12 @@ const AkmaljonCart = () => {
               alt=""
             />
           </div>
-          <div className="ml-5">
+          <div className="ml-5 mt-5">
             <h4 className="ml-4 text-[#5f5b57] cursor-pointer font-semibold hover:text-[#E99C2E] hover:duration-500">
               Latest Designed Stool and Chair
             </h4>
             <br />
-            <p className="text-[14px] lg:w-[60%] xl:w-[80%] text-[#a09e9c] xl:ml-5">
+            <p className="text-[14px] lg:w-[100%] xl:w-[80%] text-[#a09e9c] xl:ml-5">
               Edi Ut Perspiciatis Unde Omnis Iste Natusina Error Sit Voluptatem
               Accusantium Doloret Mque Laudantium, Totam Rem Aperiam.
             </p>
@@ -50,16 +50,16 @@ const AkmaljonCart = () => {
           </div>
         </div>
       </div>
-      <div className="pb-0 xl:py-12 w-full xl:h-[290px] md:w-[50%] m-auto xl:w-[34%] bg-[#f8f9fc] flex justify-center">
+      <div className="pb-0 xl:py-14 h-[280px] pt-14 w-[100%] xl:h-[300px] lg:w-[100%] md:w-[50%] m-auto xl:w-[34%] bg-[#f8f9fc] flex justify-center">
         <div className="text-center">
-          <div className="flex justify-center xl:-mt-11">
+          <div className="flex justify-center -mt-12 w-[80%] m-auto">
             <img src={Rasm3} alt="" />
           </div>
           <div className="my-9">
-            <h4>Hanging Lamp</h4>
+            <h4>Arm Chair</h4>
           </div>
           <div>
-            <p className="text-[14px] w-[90%] text-[#a09e9c] xl:mx-auto">
+            <p className="text-[14px] text-[#a09e9c] mx-auto w-[92%]">
               Nemo enim ipsam voluptatem quia volu ptas sit asperna aut odit aut
             </p>
           </div>
